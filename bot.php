2a2f78d06e418e20a02116deb219b6f0fbe463d9
patipat <patipat_c@cmu.ac.th>
2017-01-04 19:31:56 +0700

    Adding reply message about userId

--- bot.php
+++ bot.php
@@ -18,9 +18,16 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $text
+				{
+					'type' => 'text',
+					'text' => $text
+				},
+				{
+          "type" => "text",
+          "text" => $event['source']['userId']
+        }
 			];
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -41,8 +48,6 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			echo "User id is : ";
-			echo $event[source][userId];
 		}
 	}
 }
